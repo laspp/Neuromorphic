8b804372f211cc690d95d114cc6f97af92b6efcc
andjela9 <62554740+andjela9@users.noreply.github.com>
2024-04-05 14:22:51 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+from model import neuron
+
+input_n = 4
+ms = 1000
+spike_train_black = [1 if i%100==0 else 0 for i in range(ms)]
+spike_train_white = [1 if i%300==0 else 0 for i in range(ms)]
+I = [1 for _ in range(1000)]
+print(I)
+eq_input = neuron.run_Izikevic(I)                  # TODO: sta ce biti struja I
+
+input_layer = []
+for i in range(input_n):
+    input_layer.append(neuron.run_Izikevic(I))
